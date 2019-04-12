d60a907b1786b63ace4524423d6abdfb2ae02d1f
Daniel Tipping <daniel@oldreliable.tech>
2019-04-12 18:45:28 +0100

    Windows: add CMAKE_MODULE_PATH

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,7 @@ set TMPDIR=%LOCALAPPDATA%\Temp
 
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -D CMAKE_MODULE_PATH=%SRC_DIR%/cmake ^
       -D CMAKE_BUILD_TYPE=Release ^
       %SRC_DIR%/metview/src/codes_ui
 if errorlevel 1 exit 1
