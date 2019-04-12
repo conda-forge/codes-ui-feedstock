6dbae48a283a54debf167d437f3d0cc1b1f81281
Daniel Tipping <daniel@oldreliable.tech>
2019-04-12 17:28:01 +0100

    Remove LIBRARY_SEARCH_VAR export

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,6 @@
 
 set -e # Abort on error.
 
-if [[ $(uname) == Darwin ]]; then
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-elif [[ $(uname) == Linux ]]; then
-    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
-fi
-
 export PYTHON=
 export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
 export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
