26ee4d53dbebc46cca0717516fd75e838ac5f1c0
Daniel Tipping <daniel@oldreliable.tech>
2019-04-13 11:54:27 +0100

    Windows: replace backslashes in CMAKE_MODULE_PATH
    To avoid an 'Invalid character escape' error.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,13 @@ cd ../build
 
 set TMPDIR=%LOCALAPPDATA%\Temp
 
+:: replace backslash with forward slash to avoid an 'Invalid character escape' error.
+set CMAKE_MODULE_PATH=%SRC_DIR%/cmake
+set CMAKE_MODULE_PATH=%CMAKE_MODULE_PATH:\=/%
+
 cmake -G "NMake Makefiles" ^
       -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-      -D CMAKE_MODULE_PATH=%SRC_DIR%/cmake ^
+      -D CMAKE_MODULE_PATH=%CMAKE_MODULE_PATH% ^
       -D CMAKE_BUILD_TYPE=Release ^
       %SRC_DIR%/metview/src/codes_ui
 if errorlevel 1 exit 1
