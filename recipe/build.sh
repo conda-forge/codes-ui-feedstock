9749043908c5eb481bf74e301097a8da1caf1dca
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-28 00:48:10 -0700

    update and swtich to v1

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,28 +2,14 @@
 
 set -e # Abort on error.
 
-export PYTHON=
-export LDFLAGS="$LDFLAGS -L$PREFIX/lib -Wl,-rpath,$PREFIX/lib"
-export CFLAGS="$CFLAGS -fPIC -I$PREFIX/include"
-
-if [[ $(uname) == Linux ]]; then
-  # workaround for https://github.com/conda-forge/qt-feedstock/issues/123
-  sed -i 's|_qt5gui_find_extra_libs(EGL.*)|_qt5gui_find_extra_libs(EGL "EGL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
-  sed -i 's|_qt5gui_find_extra_libs(OPENGL.*)|_qt5gui_find_extra_libs(OPENGL "GL" "" "")|g' $PREFIX/lib/cmake/Qt5Gui/Qt5GuiConfigExtras.cmake
-fi
-
-
-CODESUI_SRC_DIR="$SRC_DIR"
-mkdir ../build && cd ../build
-
 # Needed for build
 export TMPDIR=/tmp/
 
-cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-      -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_MODULE_PATH=$SRC_DIR/cmake \
-      $CODESUI_SRC_DIR
+cmake -B build -S ${SRC_DIR} \
+    --debug-find-pkg=Threads \
+    -D CMAKE_BUILD_TYPE=Release \
+    ${CMAKE_ARGS}
 
-make -j $CPU_COUNT
-
-make install
+    # -D CMAKE_MODULE_PATH=$SRC_DIR/cmake \
+cmake --build build -j ${CPU_COUNT}
+cmake --install build
