7c227ea1939b6f8370ec66289f123b4b87e6ca28
Daniel Nachun <daniel.nachun@gmail.com>
2026-03-28 01:03:22 -0700

    script cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-set -e # Abort on error.
-
-# Needed for build
-export TMPDIR=/tmp/
+set -o xtrace -o nounset -o pipefail -o errexit
 
 cmake -B build -S ${SRC_DIR} \
     --debug-find-pkg=Threads \
     -D CMAKE_BUILD_TYPE=Release \
     ${CMAKE_ARGS}
 
-    # -D CMAKE_MODULE_PATH=$SRC_DIR/cmake \
 cmake --build build -j ${CPU_COUNT}
 cmake --install build
