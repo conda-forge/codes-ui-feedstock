08cc58d7e4a12a02d86b4aa1b419d9abb16ce347
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2026-04-02 10:08:20 +0100

    Enable C++17 on Windows build

--- recipe/build.bat
+++ recipe/build.bat
@@ -3,6 +3,9 @@
 
 cmake -B build -S %SRC_DIR% -G Ninja ^
       -D CMAKE_BUILD_TYPE=Release ^
+      -D CMAKE_CXX_STANDARD=17 ^
+      -D CMAKE_CXX_EXTENSIONS=OFF ^
+      -D CMAKE_CXX_FLAGS="/Zc:__cplusplus" ^
       %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
