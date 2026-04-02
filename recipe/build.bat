5417cca484455ff106e76c1e7658bc8a13d168ef
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2026-04-02 10:15:49 +0100

    Windows - stricter compliance

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,7 +5,7 @@ cmake -B build -S %SRC_DIR% -G Ninja ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D CMAKE_CXX_STANDARD=17 ^
       -D CMAKE_CXX_EXTENSIONS=OFF ^
-      -D CMAKE_CXX_FLAGS="/Zc:__cplusplus" ^
+      -D CMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" ^
       %CMAKE_ARGS%
 if errorlevel 1 exit 1
 
