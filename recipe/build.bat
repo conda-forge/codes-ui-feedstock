b3174597b8ffbe87dfac330060582d217c17fa30
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2026-04-02 16:02:22 +0100

    Enable Metview support for Windows in build script

--- recipe/build.bat
+++ recipe/build.bat
@@ -4,6 +4,7 @@
 cmake -B build -S %SRC_DIR% -G Ninja ^
       -D CMAKE_BUILD_TYPE=Release ^
       -D _WIN32=1 ^
+      -D METVIEW_ON_WINDOWS=1 ^
       -D CMAKE_CXX_STANDARD=17 ^
       -D CMAKE_CXX_EXTENSIONS=OFF ^
       -D CMAKE_CXX_FLAGS="/Zc:__cplusplus /permissive-" ^
